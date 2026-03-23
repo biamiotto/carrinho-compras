3d30d080d3893350225ba3292c5ee229f25e13c3
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2026-03-23 16:03:31 -0300

    centraliza div

--- carrinho-compras/src/App.jsx
+++ carrinho-compras/src/App.jsx
@@ -5,21 +5,21 @@ function App() {
     const itensNoCarrinho = [
         {
             id: 1,
-            nome: 'Teclado Mecânico RGB',
+            nome: 'Teclado Mecânico',
             preco: 250.0,
             quantidade: 1,
             imagem: 'https://images.kabum.com.br/produtos/fotos/472044/teclado-mecanico-gamer-kbm-gaming-tg600-preto-60-e-abnt2-rgb-switch-gateron-blue-kgtg600ptaz_1709825263_gg.jpg',
         },
         {
             id: 2,
-            nome: 'Mouse Gamer 12000 DPI',
+            nome: 'Mouse Gamer',
             preco: 120.0,
             quantidade: 2,
             imagem: 'https://cdn.awsli.com.br/600x450/563/563019/produto/275778743/8acc2fcc135d6a80b5449fac5372c028-fridm6wqan.jpeg',
         },
         {
             id: 3,
-            nome: "Monitor 144Hz 24'",
+            nome: "Monitor 24 polegadas",
             preco: 800.0,
             quantidade: 1,
             imagem: 'https://images.kabum.com.br/produtos/fotos/sync_mirakl/471635/Monitor-Gamer-Mitsushiba-24-Polegadas-LED-1920-x-1080-60hz-Multim-dia-FHD-HDMI-VGA_1697837887_gg.jpg',
